4619cecaa7348bb7c89fbec183d660c7aade3317
David Ronk <dronk@compendium.com>
2019-04-26 14:30:43 -0400

    it's alive!! - docraptor sync - pdf reader - sorting - multiple urls

--- app/controllers/pdf_metadata_controller.rb
+++ app/controllers/pdf_metadata_controller.rb
@@ -1,45 +1,72 @@
 class PdfMetadataController < ApplicationController
+  
   DocRaptor.configure do |config|
     config.username = "YOUR_API_KEY_HERE"
     # config.debugging = true
   end
 
-  before_action :set_default_response_format
-
+  # takes an array of urls and returns json metadata for each
   def index
-    docraptor = DocRaptor::DocApi.new
-    response = docraptor.create_doc(
-      test:             true,                                         # test documents are free but watermarked
-      document_content: "<html><body>Hello World</body></html>",      # supply content directly
-      # document_url:   "http://docraptor.com/examples/invoice.html", # or use a url
-      name:             "docraptor-ruby.pdf",                         # help you find a document later
-      document_type:    "pdf",                                        # pdf or xls or xlsx
-      # javascript:       true,                                       # enable JavaScript processing
-      # prince_options: {
-      #   media: "screen",                                            # use screen styles instead of print styles
-      #   # baseurl: "http://hello.com",                                # pretend URL when using document_content
-      # },
-    )
-    puts response.class
 
+    results = {}
+    if params[:urls].present?
+      params[:urls].each do |url|
 
+        # use docraptor to convert html > pdf
+        pdf_str = docraptor_sync url
 
-    # reader PDF::Reader.new(response)
-    StringIO.open(response, "rb") do |io|
-      reader = PDF::Reader.new(io)
-      puts "pdf_version: #{reader.pdf_version}"
-      puts "info #{reader.info}"
-      puts "metadata #{reader.metadata}"
-      puts "page_count #{reader.page_count}"
+        # read the metadata from the pdf
+        meta = metadata_from_pdf(url, pdf_str)
+
+        # populate results grouped by page_count
+        page_count = meta[:page_count]
+        results[page_count] = [] if results[page_count].nil?
+        results[page_count] << meta
+      end
+
+      # sort each page group by url
+      results.each_value do |same_size_pdfs|
+        same_size_pdfs.sort_by! {|doc| doc[:url]}
+      end
     end
 
-    # puts reader.pdf_version
-    # puts reader.info
-    # puts reader.metadata
-    # puts reader.page_count
+    results[0] = {
+      :params => params[:urls].size,
+      :urls => params[:urls]
+    }
+
+    render :json => results
   end
 
   private
+  def docraptor_sync(url)
+    docraptor = DocRaptor::DocApi.new
+    docraptor.create_doc(
+      test:             true,                                         # test documents are free but watermarked
+      document_url:   url,
+      # name:             "docraptor-ruby.pdf",                         # help you find a document later
+      document_type:    "pdf",                                        # pdf or xls or xlsx
+      # javascript:       true,                                       # enable JavaScript processing
+      prince_options: {
+      #   media: "screen",                                            # use screen styles instead of print styles
+      #   # baseurl: "http://hello.com",                                # pretend URL when using document_content
+      },
+    )
+  end
+
+  def metadata_from_pdf(url, pdf_str)
+    meta = {}
+    StringIO.open(pdf_str, "rb") do |io|
+      reader = PDF::Reader.new(io)
+      meta[:url] = url
+      meta[:pdf_version] = reader.pdf_version
+      meta[:info] = reader.info
+      meta[:metadata] = reader.metadata
+      meta[:page_count] = reader.page_count
+    end
+    meta
+  end
+
   def set_default_response_format
     request.format = :json
   end
