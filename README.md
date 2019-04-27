d89953ec679f2aacc8c5090d00e8133097f41b9c
David Ronk <dronk@compendium.com>
2019-04-26 21:32:25 -0400

    Updated error handling - Decided to not silently fail urls; instead list out failed urls in error section

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Running locally
+* Run against Ruby 2.4.6.
+* To run: clone the repo, run `bundle install`, then `bundle exec rails server`
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+## Running live
+* hosted at [https://dmr-raptor-hw.herokuapp.com/pdf_metadata]
+* pass `urls` to the above endpoint to convert the page to pdf then display metadata about those pdfs
+* ex: [https://dmr-raptor-hw.herokuapp.com/pdf_metadata?urls[]=http://docraptor.com/examples/invoice.html&urls[]=http://google.com&urls[]=https://www.google.com]
--- app/controllers/pdf_metadata_controller.rb
+++ app/controllers/pdf_metadata_controller.rb
@@ -4,13 +4,16 @@ class PdfMetadataController < ApplicationController
   def index
 
     results = {}
+    errors = []
     if params[:urls].present?
 
       @docraptor = DocRaptor::DocApi.new
       docraptor_futures = []
       
+      # if urls param isn't an array make it one
+      urls = *params[:urls]
       # create async docraptor pdf requests for urls
-      params[:urls].sort.each do |url|
+      urls.sort.each do |url|
         begin
           docraptor_futures << {
             :url => url,
@@ -18,30 +21,31 @@ class PdfMetadataController < ApplicationController
           }
         rescue => e
           puts "Error creating async pdf request: #{e.message}"
+          errors << "Error creating pdf request for: #{url}"
         end
       end
 
       docraptor_futures.each do |future_doc|
-        # wait for and process requested pdf
-        pdf_str = process_future(future_doc[:async])
+        begin
+          # wait for and process requested pdf
+          pdf_str = process_future(future_doc[:async])
 
-        if pdf_str.present?
           # read the metadata from the pdf
           meta = metadata_from_pdf(future_doc[:url], pdf_str)
 
-          if meta.present?
-            # populate results grouped by page_count
-            page_count = meta[:page_count]
-            results[page_count] = [] if results[page_count].nil?
-            results[page_count] << meta
-          end
+          # populate results grouped by page_count
+          page_count = meta[:page_count]
+          results[page_count] = [] if results[page_count].nil?
+          results[page_count] << meta
+        rescue => e
+          puts "Error processing PDF for: #{future_doc[:url]}\n#{e.message}"
+          errors << "Error processing pdf for #{future_doc[:url]}"
         end
       end
     else
-      # do something here
-      results[:error] = "missing 'urls' parameter"
+      errors << "missing or empty 'urls' parameter"
     end
-
+    results[:errors] = errors if errors.present?
     render :json => results
   end
 
@@ -56,42 +60,31 @@ class PdfMetadataController < ApplicationController
   end
 
   def process_future(create_response)
-    begin
-      loop do
-        status_response = @docraptor.get_async_doc_status(create_response.status_id)
-        puts "doc status: #{status_response.status}"
-        case status_response.status
-        when "completed"
-          return @docraptor.get_async_doc(status_response.download_id)
-        when "failed"
-          puts "FAILED: #{status_response}"
-          break
-        else
-          sleep 1
-        end
+    loop do
+      status_response = @docraptor.get_async_doc_status(create_response.status_id)
+      puts "doc status: #{status_response.status}"
+      case status_response.status
+      when "completed"
+        return @docraptor.get_async_doc(status_response.download_id)
+      when "failed"
+        puts "FAILED: #{status_response}"
+        raise "Failed processing async pdf response: #{status_response}"
+      else
+        sleep 1
       end
-    rescue => e
-      puts "Error processing async pdf request: #{e.message}"
     end
-    nil
   end
 
   def metadata_from_pdf(url, pdf_str)
-    # meta = nil
-    begin
-      return StringIO.open(pdf_str, "rb") do |io|
-        reader = PDF::Reader.new(io)
-        {
-          :url => url,
-          :pdf_version => reader.pdf_version,
-          :info => reader.info,
-          :metadata => reader.metadata,
-          :page_count => reader.page_count
-        }
-      end
-    rescue => e
-      puts "Error reading PDF from #{url}: #{e.message}"
+    StringIO.open(pdf_str, "rb") do |io|
+      reader = PDF::Reader.new(io)
+      {
+        :url => url,
+        :pdf_version => reader.pdf_version,
+        :info => reader.info,
+        :metadata => reader.metadata,
+        :page_count => reader.page_count
+      }
     end
-    nil
   end
 end
